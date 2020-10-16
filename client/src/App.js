aba39eecb604e7278452ab57443e6bb8317e5886
Arshad-97 <agmarshad97@gmail.com>
2020-10-16 19:54:43 +0530

    Step 8 : Created Header for frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/layout/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>hello react</h1>
+      <Header />
     </div>
   );
 }
