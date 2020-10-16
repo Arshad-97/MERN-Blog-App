a9247802359cb5dfec851096988513677766abd5
Arshad-97 <agmarshad97@gmail.com>
2020-10-16 21:16:01 +0530

    Step 9 :Created  Navigation for forntent

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/layout/Header';
+import Navbar from './components/layout/NavBar';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navbar />
     </div>
   );
 }
