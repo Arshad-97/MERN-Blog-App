cab38cf2e1915e3164c42a0affb5bafb7444f4b6
Arshad-97 <agmarshad97@gmail.com>
2020-10-17 19:29:17 +0530

    Step 15 : Successfully created loading gif image

--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import styled from "styled-components";
+import spinner from '././spinner-1.gif';
 
 const Articles = ({ posts }) => {
     return (
         <MainContainer>
-            {posts.map((articles, key) => (
-                <div className="container">
-                    <h2>{articles.title}</h2>
-                    <p>{articles.article}</p>
-                    <span className="badge badge-secondary p-2">{articles.authorname}</span>
-                    <div className="row my-5">
-                        <div className="col-sm-2">
-                            <a className="btn btn-outline-success">Edit Article</a>
+            {!posts.length ? (<img src={spinner} alt="loading..." />
+            ) : (
+                    posts.map((articles, key) => (
+                        <div className="container">
+                            <h2>{articles.title}</h2>
+                            <p>{articles.article}</p>
+                            <span className="badge badge-secondary p-2">{articles.authorname}</span>
+                            <div className="row my-5">
+                                <div className="col-sm-2">
+                                    <a className="btn btn-outline-success">Edit Article</a>
+                                </div>
+                                <div className="col-sm-2">
+                                    <a className="btn btn-outline-danger">Delete Article</a>
+                                </div>
+                            </div>
                         </div>
-                        <div className="col-sm-2">
-                            <a className="btn btn-outline-danger">Delete Article</a>
-                        </div>
-                    </div>
-                </div>
-            ))}
+                    ))
+                )}
         </MainContainer>
     )
 }
@@ -28,4 +32,10 @@ export default Articles;
 // MainContainer
 const MainContainer = styled.div`
     margin : 7rem;
+
+    img {
+        width: 10rem;
+        display: block;
+        margin: 0 auto;
+    }
 `;
